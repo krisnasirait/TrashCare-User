d8f72a8af1d9edf64071952578865ea888e25589
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-05-31 18:48:37 +0700

    added welcome activity

--- app/src/main/java/com/trashcare/user/ui/inputdata/InputDataActivity.kt
+++ app/src/main/java/com/trashcare/user/ui/inputdata/InputDataActivity.kt
@@ -1,12 +1,21 @@
 package com.trashcare.user.ui.inputdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.trashcare.user.R
+import com.trashcare.user.databinding.ActivityInputDataBinding
+import com.trashcare.user.ui.welcome.WelcomeActivity
 
 class InputDataActivity : AppCompatActivity() {
+    private lateinit var binding  : ActivityInputDataBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_data)
+        binding = ActivityInputDataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmitData.setOnClickListener {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/trashcare/user/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.trashcare.user.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trashcare.user.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
