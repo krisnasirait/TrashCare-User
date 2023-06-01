86a9436ba11781f35c4242e9b12747bc700986f0
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-06-01 15:14:08 +0700

    Fixed a bug in the bottom nav

--- app/src/main/java/com/trashcare/user/MainActivity.kt
+++ app/src/main/java/com/trashcare/user/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.bottomNavigation
+        val navView: BottomNavigationView = binding.bottomNav
         val navController = findNavController(R.id.nav_host_fragment)
 
         navView.setupWithNavController(navController)
