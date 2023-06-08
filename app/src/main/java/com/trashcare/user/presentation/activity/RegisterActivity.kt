1f6f8acda03b3ed0223e58a3dcb1f0b901f6d9ec
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-06-08 11:13:40 +0700

    create activity registers

--- app/src/main/java/com/trashcare/user/presentation/activity/RegisterActivity.kt
+++ app/src/main/java/com/trashcare/user/presentation/activity/RegisterActivity.kt
@@ -1,22 +1,22 @@
-@file:Suppress("DEPRECATION")
-
 package com.trashcare.user.presentation.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.trashcare.user.databinding.ActivityOnboardPageBinding
+import com.trashcare.user.R
+import com.trashcare.user.databinding.ActivityRegisterBinding
 
-class OnboardPageActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityOnboardPageBinding
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOnboardPageBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGetStarted.setOnClickListener {
+        // Sementara
+        binding.btnRegister.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
