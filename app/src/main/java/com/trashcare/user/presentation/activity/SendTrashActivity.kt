452608966c4585fd79520d58dd414c6157ed3c71
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-06-08 16:52:56 +0700

    create maps activity, using api key

--- app/src/main/java/com/trashcare/user/presentation/activity/SendTrashActivity.kt
+++ app/src/main/java/com/trashcare/user/presentation/activity/SendTrashActivity.kt
@@ -1,12 +1,22 @@
 package com.trashcare.user.presentation.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trashcare.user.R
+import com.trashcare.user.databinding.ActivitySendTrashBinding
 
 class SendTrashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySendTrashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_send_trash)
+        binding = ActivitySendTrashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSelectLocation.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
