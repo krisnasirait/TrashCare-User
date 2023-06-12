6a985cfbfec6b8f68cdc5cea06c3b7a13023ffb6
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-06-12 16:45:41 +0700

    get jvm from 1.8 to 17

--- app/src/main/java/com/trashcare/user/presentation/activity/LoginActivity.kt
+++ app/src/main/java/com/trashcare/user/presentation/activity/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trashcare.user.R
 import com.trashcare.user.databinding.ActivityLoginBinding
+import com.trashcare.user.utils.EmailValidation.isEmailValid
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,14 +15,38 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvRegLog.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
+        setUpLoginAction()
 
+    }
+
+
+    private fun setUpLoginAction() {
         binding.btnLogin.setOnClickListener {
-            val intent =  Intent(this, WelcomeActivity::class.java)
-            startActivity(intent)
+            val email = binding.edEmailLogin.text.toString()
+            val password = binding.edPasswordLogin.text.toString()
+            when {
+                email.isEmpty() -> {
+                    binding.edEmailLogin.error = resources.getString(R.string.emailEmptyError)
+                }
+                password.isEmpty() -> {
+                    binding.edPasswordLogin.error = resources.getString(R.string.passwordEmptyError)
+                }
+                !isEmailValid(email) -> {
+                    binding.edEmailLogin.error = resources.getString(R.string.formatEmailWrong)
+                }
+                password.length < 6 -> {
+                    binding.edPasswordLogin.error = resources.getString(R.string.lengthPasswordWrong)
+                }
+                else -> {
+                    val intent =  Intent(this, WelcomeActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            binding.tvRegLog.setOnClickListener {
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
